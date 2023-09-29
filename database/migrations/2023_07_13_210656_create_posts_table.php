87d610e4c079c41a9574184e4fea4456173d9ab2
Koji Sato <utautaouka@gmail.com>
2023-09-29 22:40:22 +0900

    is_publishの追加

--- database/migrations/2023_07_13_210656_create_posts_table.php
+++ database/migrations/2023_07_13_210656_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->boolean('is_publish')->default(true);
             $table->text('content');
             $table->timestamps();
         });
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -10,33 +10,43 @@
             </div>
             <div class="-my-8 divide-y-2 divide-gray-100">
                 @foreach ($posts as $key => $post)
-                    <div class="py-8 flex flex-wrap md:flex-nowrap">
-                        <div class="w-full md:w-40 md:mb-0 mb-3 flex-shrink-0 flex flex-col">
-                            <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                            <span class="mt-1 text-gray-500 text-sm">
-                                @if ($post->updated_at > $post->created_at)
-                                    {{ $post->updated_at->format('Y-m-d') }}
-                                @else
-                                    {{ $post->created_at->format('Y-m-d') }}
-                                @endif
-                            </span>
-                        </div>
-                        <div class="w-full md:flex-1">
-                            <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{{ $post->title }}</h2>
-                            <p class="leading-relaxed">{{ $post->content }}</p>
-                            <a href="{{ route('post.edit', ['post' => $post->id]) }}" class="text-indigo-500 inline-flex items-center mt-4">Learn More
+                <div class="py-8 flex flex-wrap">
+                    <div class="w-full md:w-64 md:mb-0 mb-3 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-gray-700">CATEGORY</span>
+                        <span class="mt-1 text-gray-500 text-sm">
+                            @if ($post->updated_at > $post->created_at)
+                            {{ $post->updated_at->format('Y-m-d') }}
+                            @else
+                            {{ $post->created_at->format('Y-m-d') }}
+                            @endif
+                        </span>
+                    </div>
+                    <div class="w-full md:flex-1">
+                        <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{{ $post->title }}</h2>
+                        <p class="leading-relaxed">{{ $post->content }}</p>
+                        <a href="{{ route('post.edit', ['post' => $post->id]) }}" class="text-indigo-500 inline-flex items-center mt-4">Learn More
                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
                             </svg>
-                            </a>
-                        </div>
-                        <form action="{{ route('post.destroy', ['post' => $post->id]) }}" method="post" class="w-full md:w-32 md:mb-0 flex items-center justify-end">
+                        </a>
+                    </div>
+                    <div class="w-full md:flex-1 md:mb-0 flex items-center gap-4 justify-end">
+                        <form id="update_is_publish_{{ $post->id }}" action="{{ route('post.update', ['post' => $post->id]) }}" method="post">
+                            @csrf
+                            @method('put')
+                            <select name="is_publish" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-4 pr-8 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" data-id="{{ $post->id }}" onchange="confirmPublish(this)">
+                                <option value="0" {{ $post->is_publish === 0 ? 'selected' : '' }}>非公開</option>
+                                <option value="1" {{ $post->is_publish === 1 ? 'selected' : '' }}>公開</option>
+                            </select>
+                        </form>
+                        <form id="delete_{{ $post->id }}" action="{{ route('post.destroy', ['post' => $post->id]) }}" method="post" class="">
                             @csrf
                             @method('delete')
-                            <button class="px-6 py-2 bg-red-600 text-white rounded-md text-sm">削除</button>
+                            <button class="px-4 py-1 bg-red-600 text-white rounded-md text-sm whitespace-nowrap" data-id="{{ $post->id }}" onclick="deletePost(this)">削除</button>
                         </form>
                     </div>
+                </div>
                 @endforeach
             </div>
             <div class="mt-12">
@@ -44,5 +54,20 @@
             </div>
         </div>
     </section>
-
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもよいですか？')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+        function confirmPublish(selectElement) {
+            'use strict';
+            let selectedOption = selectElement.options[selectElement.selectedIndex].text;
+            let confirmationMessage = `この投稿を${selectedOption}に変更してもよいですか？`;
+            if (confirm(confirmationMessage)) {
+                document.getElementById('update_is_publish_' + selectElement.dataset.id).submit();
+            }
+        }
+    </script>
 </x-app-layout>
